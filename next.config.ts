67342269059676001fb20893f8b590099aba486b
Natanael <n2ilva@users.noreply.github.com>
2026-01-16 00:32:50 -0300

    fix: Remove basePath and assetPrefix for Netlify deployment (root path) and enable server API routes

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,12 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
+
 
 const nextConfig: NextConfig = {
-  output: "export",
+  /* config options here */
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? "/SaraRamos" : "",
-  assetPrefix: isProd ? "/SaraRamos" : "",
 };
 
 export default nextConfig;
